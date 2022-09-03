b6c2a544a85d19d4fe2c44e7ae624a3cf1f68a45
Alexandru Raileanu <alexandru98raileanu@yahoo.com>
2022-09-02 23:50:46 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let btnToggle = document.getElementById("toggle-content");
+let btnAdd = document.getElementById("add-content");
+let btnRemove = document.getElementById("remove-content");
+
+btnToggle.addEventListener("click", onClickBtnToggle);
+btnAdd.addEventListener("click", onClickBtnAdd);
+btnRemove.addEventListener("click", onClickBtnRemove);
+
+
+function onClickBtnToggle() {
+    let links = document.getElementById("link-container");
+
+    links.classList.toggle("d-none");
+}
+
+function onClickBtnAdd() {
+    let links = document.getElementById("link-container");
+
+    // adaug un link
+    // 1. creez un <a>
+    let newLink = document.createElement("a");
+    newLink.setAttribute("href", "#");
+    newLink.classList.add("card-link");
+    newLink.innerHTML = "Un alt link";
+
+    // 2. adaug in pagina
+    links.appendChild(newLink);
+}
+
+function onClickBtnRemove() {
+    let links = document.getElementById("link-container");
+    let element = links.firstElementChild;
+
+    if (element) {
+        links.removeChild(element);
+    }
+}
